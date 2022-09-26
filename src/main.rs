a89566b31d5aa85920b428bcb9d83c8ac4a7aa8e
Jeremy Schlatter <jeremy@jeremyschlatter.com>
2022-09-25 19:32:42 -0700

    hello ggez

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use ggez::*;
+
+struct State {
+    dt: std::time::Duration,
+    n: u32,
+}
+
+impl ggez::event::EventHandler<GameError> for State {
+    fn update(&mut self, ctx: &mut Context) -> GameResult {
+        self.dt = ctx.time.delta();
+        self.n += 1;
+        Ok(())
+    }
+    fn draw(&mut self, ctx: &mut Context) -> GameResult {
+        if self.n % 10 == 0 {
+            println!("Hello ggez! dt = {}ms", self.dt.as_millis());
+        }
+        Ok(())
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let state = State {
+        dt: std::time::Duration::new(0, 0),
+        n: 0,
+    };
+    let c = conf::Conf::new();
+    let (ctx, event_loop) = ContextBuilder::new("hello_ggez", "Jeremy")
+        .default_conf(c)
+        .build()
+        .unwrap();
+
+    event::run(ctx, event_loop, state);
 }
