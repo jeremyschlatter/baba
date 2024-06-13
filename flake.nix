d96211cc05d269291ac718749247a1334da97806
Jeremy Schlatter <jeremy@jeremyschlatter.com>
2024-06-13 16:19:52 -0700

    flake: try getting flakes from registry

--- flake.nix
+++ flake.nix
@@ -1,9 +1,4 @@
 {
-  inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/release-24.05;
-    flake-utils.url = github:numtide/flake-utils;
-  };
-
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
     with nixpkgs.legacyPackages.${system};
@@ -13,7 +8,7 @@
       devShell = stdenvNoCC.mkDerivation {
         name = "shell";
         buildInputs = lib.attrsets.mapAttrsToList writeShellScriptBin scripts ++ [
-
+          cargo
         ];
       };
     });
