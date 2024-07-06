c08a284adbe100703da679aaf8f43e7d0379e1b7
Jeremy Schlatter <jeremy@jeremyschlatter.com>
2024-07-05 20:36:40 -0700

    add all abbreviations to .nvim.lua

--- .nvim.lua
+++ .nvim.lua
@@ -1,8 +1,40 @@
+abbrevs = {
+  you='✥',
+  stop='⊘',
+  push='↦',
+  win='✓',
+  sink='≉',
+  defeat='⩍',
+  hot='⌇',
+  melt='⌢',
+  move='→',
+  shut='⨶',
+  open='⧜',
+  float='⚲',
+  weak='_',
+  tele='*',
+  pull='↣',
+  shift='^',
+  swap='↔',
+
+  up='⇧',
+  down='⇩',
+  right='⇨',
+  left='⇦',
+
+  is='=',
+  ["not"]='¬',
+  ["and"]='&',
+  has='~',
+  text='@',
+  empty='?',
+}
 vim.api.nvim_create_autocmd({'BufNewFile', 'BufRead'}, {
   pattern = {'*.txt'},
   callback = function()
-    vim.cmd('iabbr <buffer> stop ⊘')
-    vim.b.unfo_ftplugin = 'unabbr stop'
+    for k,v in pairs(abbrevs) do
+      vim.cmd('iabbr <buffer> ' .. k .. ' ' .. v)
+    end
   end
 })
 vim.cmd('doautocmd BufRead')
